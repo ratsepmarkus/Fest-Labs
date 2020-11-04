739aa9f83f57fc4ec0ff1bb74035efd607d51799
Markus Rätsep <56824783+ratsepmarkus@users.noreply.github.com>
2020-11-04 20:18:24 +0200

    Delete groups.js

--- groups.js
+++
@@ -1,7 +0,0 @@
-fetch('https://be.ta19heinsoo.itmajakas.ee/api/groups')
-.then(response => {
-    return response.json()
-}).then(data => {
-    console.log(data)
-    const info = document.getElementById('teachers')
-})
