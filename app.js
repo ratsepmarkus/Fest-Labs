0ab6c66ecbad30c03f90e087b0b6c8ef10ed8169
Markus Rätsep <56824783+ratsepmarkus@users.noreply.github.com>
2020-11-04 20:19:38 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const infoList = document.getElementById('infoList');
+const searchBar = document.getElementById('searchBar');
+let info = [];
+
+// FUZZY SEARCH
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = info.filter((content) => {
+        return (
+            content.firstname.toLowerCase().includes(searchString) ||
+            content.lastname.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+// API FETCH (praegu on ainult 1 API)
+const loadCharacters = async () => {
+    try {
+        const teach = await fetch('https://be.ta19heinsoo.itmajakas.ee/api/teachers')
+        info = await teach.json();
+        
+        displayCharacters(info);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// API INFO HTML-i
+const displayCharacters = (contentAll) => {
+    const htmlString = contentAll
+        .map((content) => {
+            return `<h2 class="content">${content.firstname} ${content.lastname}</h2>`;
+        })
+        .join('');
+    infoList.innerHTML = htmlString;
+};
+
+loadCharacters();
